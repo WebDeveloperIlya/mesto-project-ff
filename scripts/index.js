6a836dc8b1bd464ba08b89a3c6b2596d9f7eb83a
Ilya Kovelskiy <ilya.kovelsky@yandex.kz>
2024-03-24 23:25:56 +0500

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,10 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('card-template');
+const cardLi = document.querySelector('.card');
+const cardDeleteButton = document.querySelector('.card__delete-button');
+const cardDescription = document.querySelector('.card__description');
+const cardTitle = document.querySelector('.card__title');
+const cardLikeButton = document.querySelector('.card__like-button');
 
 // @todo: DOM узлы
 
